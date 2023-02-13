40fe3e3501c24417c33737fb275a882301b2e1cd
bananaTiko <111482265+bananaTiko@users.noreply.github.com>
2023-02-12 21:42:01 -0800

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1074558970045411329",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
